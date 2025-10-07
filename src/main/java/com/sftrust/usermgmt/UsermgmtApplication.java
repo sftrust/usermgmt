e32b287632f44829eab7adda2e51eb3fb9499360
Siddhant Singh <sidsingh250496@gmail.com>
2025-10-07 12:08:44 +0530

    Scaffolding commit

---
+++ src/main/java/com/sftrust/usermgmt/UsermgmtApplication.java
@@ -0,0 +1,13 @@
+package com.sftrust.usermgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sftrust/usermgmt/UsermgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sftrust.usermgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
